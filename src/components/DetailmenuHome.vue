2709bdf9ca7031802735051cddb5bb8caa2fe11e
Bram-Colleman <r0820751@student.thomasmore.be>
2022-06-04 18:35:11 +0200

    details listing

--- src/components/DetailmenuHome.vue
+++ src/components/DetailmenuHome.vue
@@ -25,7 +25,7 @@ let favorite = ref(false);
                 <i class="fa-regular fa-star" v-if="favorite == false" @click="favorite = !favorite"></i>
                 <i class="fa-solid fa-star" v-if="favorite" @click="favorite = !favorite"></i>
             </div>
-            <Routerlink to="/details" class="btn">Meer info</Routerlink>
+            <RouterLink to="/details" class="btn">Meer info</RouterLink>
         </div>
     </div>
 </div>
